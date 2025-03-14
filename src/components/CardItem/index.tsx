32c4239cbfd23294ea6ccd97fd85327a3151a0c3
Phuc Pham <ppham@webagility.io>
2025-03-14 21:48:00 +0700

    Fix build error

--- src/components/CardItem/index.tsx
+++ src/components/CardItem/index.tsx
@@ -5,13 +5,17 @@ import { useNavigate } from 'react-router-dom';
 
 import { CardMedia, SxProps, Typography } from '@mui/material';
 
-import { StyledTypography, SyledCard, SyledCardContent } from './index.styled';
+import {
+  StyledCard,
+  StyledCardContent,
+  StyledTypography,
+} from './index.styled';
 
 const CardItem = ({ data, sx }: { data: ArticleType; sx?: SxProps<Theme> }) => {
   const navigate = useNavigate();
 
   return (
-    <SyledCard
+    <StyledCard
       variant="outlined"
       tabIndex={0}
       sx={{ height: '100%', ...sx }}
@@ -28,7 +32,7 @@ const CardItem = ({ data, sx }: { data: ArticleType; sx?: SxProps<Theme> }) => {
           }}
         />
       )}
-      <SyledCardContent>
+      <StyledCardContent>
         <Typography gutterBottom variant="caption">
           {data.tag.name}
         </Typography>
@@ -38,9 +42,9 @@ const CardItem = ({ data, sx }: { data: ArticleType; sx?: SxProps<Theme> }) => {
         <StyledTypography variant="body2" color="text.secondary">
           {data.description}
         </StyledTypography>
-      </SyledCardContent>
+      </StyledCardContent>
       <Author author={data.author} createdAt={data.createdAt} />
-    </SyledCard>
+    </StyledCard>
   );
 };
 
